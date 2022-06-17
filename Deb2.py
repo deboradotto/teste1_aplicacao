b30b6fd0d91bc6af53e5b9a21df459cec0a8dba0
deboradotto <75906061+deboradotto@users.noreply.github.com>
2022-06-17 17:15:14 -0300

    Update Deb2.py

--- Deb2.py
+++ Deb2.py
@@ -10,6 +10,11 @@ import streamlit as st
 
 analistas = ['Débora','Marcel', 'Raul', 'Vinícius']
 
+treinamentos = ['Filas','Onboarding', 'Template', 'Thundera','Grupo Controle','SQL','Azure Synapse','Análise Grupo','Grupo Controle + Atividade']
 
-st.selectbox('Selecione o analista: ',['Todos'] + analistas)
+
+
+analista = st.selectbox('Selecione o analista: ',['Todos'] + analistas)
+
+Treinamento = st.selectbox('Selecione o treinamento concluído: ', treinamentos)
 
